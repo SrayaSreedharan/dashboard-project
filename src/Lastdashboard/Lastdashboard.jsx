6f2f469c0e9b6c6cc94b8b3734ed12d30728cada
SRAYA SREEDHARAN <srayasreedharan111@gmail.com>
2025-07-24 21:23:44 +0530

    test

--- src/Lastdashboard/Lastdashboard.jsx
+++ src/Lastdashboard/Lastdashboard.jsx
@@ -2,26 +2,14 @@ import React from 'react';
 import Booking from './Booking';
 import Revenue from './Revenue';
 import Pending from './Pending';
+import '../App.css'; // ⬅️ add this
 
 const Lastdashboard = () => {
-    const styles = {
-  containerss: {
-    display: 'flex',
-    justifyContent: 'space-between',
-    // alignItems: 'stretch',    
-    gap: '20px',
-    padding: '20px',
-    maxWidth: '1600px',
-    margin: '0 auto',
-    flexWrap: 'nowrap'        
-  }
-};
-
   return (
-    <div style={styles.containerss}>
-      <Booking/>
+    <div className="dashboard-wrapper">
+      <Booking />
       <Revenue />
-      <Pending/>
+      <Pending />
     </div>
   );
 };
